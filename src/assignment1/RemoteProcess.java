a925233f367ffe88ba1a1d5185191c1a7ee40eb1
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-02-20 15:47:44 +0100

    Gave the remote process an ID.

--- src/assignment1/RemoteProcess.java
+++ src/assignment1/RemoteProcess.java
@@ -3,17 +3,24 @@ package assignment1;
 public class RemoteProcess implements RemoteProcess_RMI<VectorTimeStamp>{
 	
 	private VectorTimeStamp timeStamp;
+	private String processId;
 	
-	public RemoteProcess(int processes)
+	public RemoteProcess(int processes, int processId)
 	{
 		this.timeStamp = new VectorTimeStamp(processes);
+		this.processId = processId;
 	}
 	
 	public VectorTimeStamp getTime()
 	{
 		return this.timeStamp;
 	}
-	
+
+        public String getId()
+        {
+            return this.Id;
+        }
+        
 	public void send(Object messange)
 	{
 		// TODO Auto-generated method stub
