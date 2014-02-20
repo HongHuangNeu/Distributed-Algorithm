c555fba2c0174208f01002b46f127cdfc12f14e3
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-02-20 14:37:13 +0100

    Added receive method to RemoteProcess

--- src/assignment1/RemoteProcess_RMI.java
+++ src/assignment1/RemoteProcess_RMI.java
@@ -6,4 +6,5 @@ public interface RemoteProcess_RMI<T extends TimeStamp> extends Remote{
 	public T getTime();
 	
 	public void send(Object messange);
+	public void recieve(T timeStamp, Object message);
 }
