7536df4809f8e38568430e13285fdbd94b395a9a
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-02-20 14:18:04 +0100

    VectorTimestamp implementation.

---
+++ src/assignment1/VectorTimeStamp.java
@@ -0,0 +1,30 @@
+package assignment1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VectorTimeStamp implements TimeStamp<List<Integer>>{
+	
+	private List<Integer> timeStamp;
+	
+	public VectorTimeStamp(int size)
+	{
+		this.timeStamp = new ArrayList<Integer>(size);
+		
+		for(int i = 0; i < size; i++)
+		{
+			this.timeStamp.set(i, 0);
+		}
+	}
+	
+	public VectorTimeStamp(List<Integer> times)
+	{
+		this.timeStamp = new ArrayList<Integer>(times);
+	}
+	
+	public List<Integer> getTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
