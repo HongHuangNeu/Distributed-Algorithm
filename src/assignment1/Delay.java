b9a9a70bbd587ad8434a47358626f8106f8ad64d
HongHuangNeu <ground.huanghong@gmail.com>
2014-02-20 16:40:18 +0100

    Delay object

--- src/assignment1/Delay.java
+++ src/assignment1/Delay.java
@@ -14,6 +14,18 @@ public static int upperBound;
    }
     public void run()
     {
+    	int delay=(int)(Math.random()*(Delay.upperBound+1));
+    	try{
+    	Thread.sleep(delay*1000);
+    	}catch(Exception e)
+    	{}
+    	try{
     RemoteProcess receiver=(RemoteProcess) Naming.lookup("rmi://localhost:1099/"+message.getReceiverName());	
+    receiver.recieve(null, message);
+    }catch(Exception e)
+    {
+    	
+    	System.out.println("Exception in fetching the object!");
+    }
     }
 }
