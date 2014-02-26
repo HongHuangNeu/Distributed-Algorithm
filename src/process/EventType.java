99cb49a45f443278e59af7f331cd59431a24db1b
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-02-26 13:36:17 +0000

    EventType's have toString methods

---
+++ src/process/EventType.java
@@ -0,0 +1,26 @@
+package process;
+
+public enum EventType
+{
+	SEND
+	{
+		public String toString()
+		{
+			return "S";
+		}
+	},
+	RECIEVE
+	{
+		public String toString()
+		{
+			return "R";
+		}
+	},
+	INTERNAL
+	{
+		public String toString()
+		{
+			return "I";
+		}
+	}
+}
